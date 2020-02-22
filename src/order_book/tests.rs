20c3c6144f4670804b587f4da112f45fdb4331d6
Sergey Levitin <selevit@gmail.com>
2020-02-22 21:37:25 +0300

    More tests

--- src/order_book/tests.rs
+++ src/order_book/tests.rs
@@ -2,7 +2,7 @@ use super::{Deal, Order, OrderBook, Side};
 
 struct TestCase {
     existing_orders: Vec<Order>,
-    placed_order: Order,
+    taker_order: Order,
     expected_deals: Vec<Deal>,
     remaining_buys: Vec<Order>,
     remaining_sells: Vec<Order>,
@@ -10,13 +10,21 @@ struct TestCase {
 
 impl TestCase {
     fn run(self) {
-        let mut book = OrderBook::new_with_orders(self.existing_orders).unwrap();
-        let deals = book.place(self.placed_order).unwrap();
+        let mut book =
+            OrderBook::new_with_orders(self.existing_orders).unwrap();
+        let deals = book.place(self.taker_order).unwrap();
         let buys: Vec<Order> = book.buy_levels.values().cloned().collect();
         let sells: Vec<Order> = book.sell_levels.values().cloned().collect();
         assert_eq!(deals, self.expected_deals);
         assert_eq!(buys, self.remaining_buys);
         assert_eq!(sells, self.remaining_sells);
+    }
+}
+
+impl Order {
+    fn with_volume(mut self, volume: u64) -> Self {
+        self.volume = volume;
+        self
     }
 }
 
@@ -30,7 +38,7 @@ fn place_buy_order_and_fill_it_partially() {
 
     TestCase {
         existing_orders: vec![maker_order],
-        placed_order: taker_order,
+        taker_order,
         expected_deals,
         remaining_buys,
         remaining_sells,
@@ -48,7 +56,36 @@ fn place_sell_order_and_fill_it_partially() {
 
     TestCase {
         existing_orders: vec![maker_order],
-        placed_order: taker_order,
+        taker_order,
+        expected_deals,
+        remaining_buys,
+        remaining_sells,
+    }
+    .run()
+}
+
+#[test]
+fn place_sell_order_and_fill_it_fully() {
+    let existing_orders = vec![
+        Order::new(Side::Buy, 5200, 3, 1),
+        Order::new(Side::Buy, 5100, 12, 2),
+        Order::new(Side::Buy, 4700, 10, 3),
+    ];
+    let taker_order = Order::new(Side::Sell, 4800, 15, 5);
+    let expected_deals = vec![
+        Deal { taker_order, maker_order: existing_orders[0], volume: 3 },
+        Deal {
+            taker_order: taker_order.with_volume(12),
+            maker_order: existing_orders[1],
+            volume: 12,
+        },
+    ];
+    let remaining_sells = vec![];
+    let remaining_buys = vec![existing_orders[2]];
+
+    TestCase {
+        taker_order,
+        existing_orders,
         expected_deals,
         remaining_buys,
         remaining_sells,
