8f6d0db15935a59ea06c0bd623cd1f1ba9768a33
Sergey Levitin <selevit@gmail.com>
2020-02-22 19:45:21 +0300

    Add docs for structures

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,10 @@ use order_book::{Order, OrderBook, Side};
 fn main() {
     let mut order_book = OrderBook::new();
 
-    let buy_order1 = Order {
-        side: Side::Buy,
-        price: 1000,
-        volume: 10,
-        user_id: 1,
-    };
-    let buy_order2 = Order {
-        side: Side::Buy,
-        price: 1001,
-        volume: 5,
-        user_id: 1,
-    };
+    let buy_order1 = Order { side: Side::Buy, price: 1000, volume: 10, user_id: 1 };
+    let buy_order2 = Order { side: Side::Buy, price: 1001, volume: 5, user_id: 1 };
 
-    let sell_order1 = Order {
-        side: Side::Sell,
-        price: 1000,
-        volume: 22,
-        user_id: 2,
-    };
+    let sell_order1 = Order { side: Side::Sell, price: 1000, volume: 22, user_id: 2 };
 
     order_book.place(buy_order1).unwrap();
     order_book.place(buy_order2).unwrap();
--- src/order_book/mod.rs
+++ src/order_book/mod.rs
@@ -2,17 +2,26 @@ use rbtree::RBTree;
 use std::cmp::{Ord, Ordering, PartialEq, PartialOrd};
 use std::vec::Vec;
 
+///
+/// An error which can occur when placing an order
+///
 #[derive(Debug)]
 pub enum PlacingError {
     Cancelled,
 }
 
+///
+/// A side of the exchange order book (buy or sell)
+///
 #[derive(PartialEq, Debug, Clone, Copy, Eq, PartialOrd)]
 pub enum Side {
     Buy,
     Sell,
 }
 
+///
+/// An order key in the RBTree which is used for storing orders in the correct order.
+///
 #[derive(Debug, Clone, Copy, Eq, PartialEq, PartialOrd)]
 struct TreeKey {
     side: Side,
@@ -20,6 +29,11 @@ struct TreeKey {
     seq_id: u64,
 }
 
+///
+/// Buy orders with higher price go first.
+/// Sell orders with higher price go last.
+/// If prices are equal, we order by sequence id (placing ordering).
+///
 impl Ord for TreeKey {
     fn cmp(&self, other: &TreeKey) -> Ordering {
         let cmp_result = self.price.cmp(&other.price);
@@ -32,6 +46,10 @@ impl Ord for TreeKey {
     }
 }
 
+///
+/// An exchange order for buying or selling assets.
+/// All prices and volumes are present as integers in base values (e.g. Satoshi or Wei)
+///
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 pub struct Order {
     pub side: Side,
@@ -42,14 +60,14 @@ pub struct Order {
 
 impl Order {
     fn _tree_key(&self, seq_id: u64) -> TreeKey {
-        TreeKey {
-            side: self.side,
-            price: self.price,
-            seq_id: seq_id,
-        }
+        TreeKey { side: self.side, price: self.price, seq_id }
     }
 }
 
+///
+/// A deal which is the result of orders filling.
+/// Stores the state of taker and maker orders before the deal.
+///
 #[derive(Debug, Eq, PartialEq)]
 pub struct Deal {
     taker_order: Order,
@@ -59,14 +77,14 @@ pub struct Deal {
 
 impl Deal {
     fn new(taker_order: Order, maker_order: Order, volume: u64) -> Deal {
-        Deal {
-            taker_order: taker_order,
-            maker_order: maker_order,
-            volume: volume,
-        }
+        Deal { taker_order, maker_order, volume }
     }
 }
 
+///
+/// A trading order book which provides the functionality
+/// for matching and filling exchange orders.
+///
 #[derive(Debug)]
 pub struct OrderBook {
     next_seq_id: u64,
@@ -74,15 +92,25 @@ pub struct OrderBook {
     sell_levels: RBTree<TreeKey, Order>,
 }
 
+impl Default for OrderBook {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl OrderBook {
-    pub fn new() -> OrderBook {
-        OrderBook {
-            next_seq_id: 0,
-            buy_levels: RBTree::new(),
-            sell_levels: RBTree::new(),
-        }
+    ///
+    /// Creates new empty order book
+    ///
+    pub fn new() -> Self {
+        OrderBook { next_seq_id: 0, buy_levels: RBTree::new(), sell_levels: RBTree::new() }
     }
 
+    ///
+    /// Places the order to the order book and tries to match it with existing orders.
+    /// Returns a list of deals if filling occured.
+    /// Returns an error if the order cannot be placed.
+    ///
     pub fn place(&mut self, order: Order) -> Result<Vec<Deal>, PlacingError> {
         let (taker_side, maker_side) = match order.side {
             Side::Buy => (&mut self.buy_levels, &mut self.sell_levels),
@@ -134,28 +162,13 @@ mod tests {
     #[test]
     fn simple_matching() {
         let mut order_book = OrderBook::new();
-        let buy_order1 = Order {
-            side: Side::Buy,
-            price: 1000,
-            volume: 10,
-            user_id: 1,
-        };
+        let buy_order1 = Order { side: Side::Buy, price: 1000, volume: 10, user_id: 1 };
         assert_eq!(order_book.place(buy_order1).unwrap().len(), 0);
 
-        let buy_order2 = Order {
-            side: Side::Buy,
-            price: 1001,
-            volume: 5,
-            user_id: 1,
-        };
+        let buy_order2 = Order { side: Side::Buy, price: 1001, volume: 5, user_id: 1 };
         assert_eq!(order_book.place(buy_order2).unwrap().len(), 0);
 
-        let sell_order1 = Order {
-            side: Side::Sell,
-            price: 1000,
-            volume: 22,
-            user_id: 2,
-        };
+        let sell_order1 = Order { side: Side::Sell, price: 1000, volume: 22, user_id: 2 };
         let deals = order_book.place(sell_order1).unwrap();
 
         assert_eq!(deals.len(), 2);
@@ -163,18 +176,8 @@ mod tests {
         assert_eq!(
             deals[0],
             Deal {
-                taker_order: Order {
-                    side: Side::Sell,
-                    price: 1000,
-                    volume: 22,
-                    user_id: 2,
-                },
-                maker_order: Order {
-                    side: Side::Buy,
-                    price: 1001,
-                    volume: 5,
-                    user_id: 1,
-                },
+                taker_order: Order { side: Side::Sell, price: 1000, volume: 22, user_id: 2 },
+                maker_order: Order { side: Side::Buy, price: 1001, volume: 5, user_id: 1 },
                 volume: 5,
             }
         );
@@ -182,18 +185,8 @@ mod tests {
         assert_eq!(
             deals[1],
             Deal {
-                taker_order: Order {
-                    side: Side::Sell,
-                    price: 1000,
-                    volume: 17,
-                    user_id: 2,
-                },
-                maker_order: Order {
-                    side: Side::Buy,
-                    price: 1000,
-                    volume: 10,
-                    user_id: 1,
-                },
+                taker_order: Order { side: Side::Sell, price: 1000, volume: 17, user_id: 2 },
+                maker_order: Order { side: Side::Buy, price: 1000, volume: 10, user_id: 1 },
                 volume: 10,
             }
         );
