c2c5782d1f0fba4bda52edb7b3aa692c9d3b3bbb
Sergey Levitin <selevit@gmail.com>
2020-02-23 03:17:35 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,16 @@ pub mod order_book;
 use order_book::{Order, OrderBook, Side};
 
 fn main() {
-    let mut order_book = OrderBook::new();
-
     let buy_order1 =
         Order { side: Side::Buy, price: 1000, volume: 10, user_id: 1 };
     let buy_order2 =
         Order { side: Side::Buy, price: 1001, volume: 5, user_id: 1 };
 
+    let mut order_book =
+        OrderBook::new_with_orders(vec![buy_order1, buy_order2]).unwrap();
+
     let sell_order1 =
         Order { side: Side::Sell, price: 1000, volume: 22, user_id: 2 };
-
-    order_book.place(buy_order1).unwrap();
-    order_book.place(buy_order2).unwrap();
 
     let deals = order_book.place(sell_order1).unwrap();
 
