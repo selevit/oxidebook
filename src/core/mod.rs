f874cfa9594b64b75945eec54bf762fff8c9f829
Sergey Levitin <selevit@gmail.com>
2020-04-05 16:34:33 +0300

    Consume inbox messages from RabbitMQ

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,5 +1,15 @@
-use crate::order_book::OrderBook;
+use crate::order_book::{Order, OrderBook, Side};
+use futures_executor::LocalPool;
+use futures_util::{future::FutureExt, stream::StreamExt};
 use std::collections::HashMap;
+
+use lapin;
+use lapin::{
+    options::{BasicAckOptions, BasicConsumeOptions, QueueDeclareOptions},
+    types::FieldTable,
+    Connection, ConnectionProperties,
+};
+use log::info;
 
 pub struct Exchange<'a> {
     pairs: HashMap<&'a str, OrderBook>,
@@ -8,6 +18,11 @@ pub struct Exchange<'a> {
 #[derive(Debug, PartialEq, Eq)]
 pub enum AddPairError {
     AlreadyExists,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum GetOrderBookError {
+    PairNotFound,
 }
 
 impl<'a> Default for Exchange<'_> {
@@ -21,11 +36,79 @@ impl<'a> Exchange<'a> {
         Exchange { pairs: HashMap::new() }
     }
 
+    pub fn get_order_book(
+        &mut self,
+        pair_name: &'a str,
+    ) -> Result<&mut OrderBook, GetOrderBookError> {
+        if let Some(order_book) = self.pairs.get_mut(pair_name) {
+            return Ok(order_book);
+        }
+        Err(GetOrderBookError::PairNotFound)
+    }
+
     pub fn add_pair(&mut self, pair_name: &'a str) -> Result<(), AddPairError> {
         if self.pairs.contains_key(pair_name) {
             return Err(AddPairError::AlreadyExists);
         }
         self.pairs.insert(pair_name, OrderBook::new());
         Ok(())
+    }
+
+    pub fn run(&mut self) -> lapin::Result<()> {
+        let mut executor = LocalPool::new();
+
+        executor.run_until(async {
+            let addr = std::env::var("AQMP_ADDR")
+                .unwrap_or_else(|_| "amqp://127.0.0.1:5672/%2f".into());
+
+            let conn =
+                Connection::connect(&addr, ConnectionProperties::default())
+                    .await?;
+
+            info!("Connected to RabbitMQ");
+
+            let consuming_channel = conn.create_channel().await?;
+            let inbox_queue = consuming_channel
+                .queue_declare(
+                    "inbox",
+                    QueueDeclareOptions::default(),
+                    FieldTable::default(),
+                )
+                .await?;
+
+            let consumer = consuming_channel
+                .clone()
+                .basic_consume(
+                    inbox_queue.name().as_str(),
+                    "core",
+                    BasicConsumeOptions::default(),
+                    FieldTable::default(),
+                )
+                .await?;
+            info!("Starting consuming inbox");
+
+            consumer
+                .for_each(move |delivery| {
+                    let delivery =
+                        delivery.expect("error caught in the inbox consumer");
+
+                    let order_book =
+                        self.pairs.get_mut("BTC_USD").expect("invalid pair");
+
+                    order_book
+                        .place(Order::new(Side::Buy, 6500, 50_000_000))
+                        .expect("placing error");
+
+                    consuming_channel
+                        .basic_ack(
+                            delivery.delivery_tag,
+                            BasicAckOptions::default(),
+                        )
+                        .map(|_| ())
+                })
+                .await;
+
+            Ok(())
+        })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 pub mod core;
 pub mod order_book;
 
+use log::info;
+
 fn main() {
+    std::env::set_var("RUST_LOG", "info");
+    env_logger::init();
+
     let mut core = core::Exchange::new();
     core.add_pair("BTC_USD").unwrap();
+
+    info!("Exchange initialized with BTC_USD");
+
+    core.run().expect("unexpected core failure");
 }
